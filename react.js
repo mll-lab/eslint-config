b3784ba3416af92c370f5e814d2453e4681bf710
Benedikt Franke <benedikt.franke@mll.com>
2022-03-18 18:03:42 +0100

    refactor: comment some rules

--- react.js
+++ react.js
@@ -6,13 +6,14 @@ module.exports = {
   ],
   plugins: ['react', 'react-hooks'],
   rules: {
+    // Not always accurate, but should be explicitly ignored in order to not trash the log
     'react-hooks/exhaustive-deps': 'error',
     'react-hooks/rules-of-hooks': 'error',
 
-    'react/default-props-match-prop-types': 'off',
+    'react/default-props-match-prop-types': 'off', // unnecessary with TypeScript
     'react/destructuring-assignment': 'off',
-    'react/forbid-prop-types': 'off',
-    'react/forbid-foreign-prop-types': 'off',
+    'react/forbid-prop-types': 'off', // unnecessary with TypeScript
+    'react/forbid-foreign-prop-types': 'off', // unnecessary with TypeScript
     'react/jsx-closing-tag-location': 'off',
     'react/jsx-filename-extension': 'off',
     'react/jsx-first-prop-new-line': ['error', 'multiline'],
@@ -21,10 +22,10 @@ module.exports = {
     'react/jsx-uses-vars': 'error',
     'react/no-access-state-in-setstate': 'error',
     'react/no-direct-mutation-state': 'error',
-    'react/no-unused-prop-types': 'off',
+    'react/no-unused-prop-types': 'off', // unnecessary with TypeScript
     'react/no-unused-state': 'error',
-    'react/prop-types': 'off',
-    'react/require-default-props': 'off',
+    'react/prop-types': 'off', // unnecessary with TypeScript
+    'react/require-default-props': 'off', // unnecessary with function components
     'react/require-extension': 'off',
     'react/self-closing-comp': 'off',
     'react/sort-comp': 'off',
